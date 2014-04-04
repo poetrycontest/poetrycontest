e3c787b0013a9be4620049ee2a69c25b74646932
demidec <errorrat+demidec@gmail.com>
2014-04-03 20:44:11 -0700

    fixes

--- sitemap.rb
+++ sitemap.rb
@@ -4,8 +4,9 @@ require 'sitemap_generator'
 SitemapGenerator::Sitemap.default_host = 'http://derkjanssenpoetrycontest.com'
 SitemapGenerator::Sitemap.create do
   add '/index', :changefreq => 'daily', :priority => 0.9
-  add '/bio', :changefreq => 'weekly', :priority => 0.4
-  add '/rules', :changefreq => 'weekly', :priority => 0.7
-  add '/submit', :changefreq => 'weekly', :priority => 0.8
+  add '/bio', :changefreq => 'weekly', :priority => 0.5
+  add '/rules', :changefreq => 'weekly', :priority => 0.6
+  add '/submit', :changefreq => 'weekly', :priority => 0.7
+  add '/', :changefreq => 'weekly', :priority => 0.8
 end
 SitemapGenerator::Sitemap.ping_search_engines
